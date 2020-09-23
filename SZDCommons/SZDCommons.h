4226fd284bda35b5d119441aec7b74271b4be135
sanzoid <15680086+sanzoid@users.noreply.github.com>
2020-09-23 17:04:14 -0400

    initial pod

---
+++ SZDCommons/SZDCommons.h
@@ -0,0 +1,19 @@
+//
+//  SZDCommons.h
+//  SZDCommons
+//
+//  Created by Sandy House on 2020-09-23.
+//  Copyright © 2020 sandzapps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SZDCommons.
+FOUNDATION_EXPORT double SZDCommonsVersionNumber;
+
+//! Project version string for SZDCommons.
+FOUNDATION_EXPORT const unsigned char SZDCommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SZDCommons/PublicHeader.h>
+
+
---
+++ SZDCommons/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  SZDCommons
+//
+//  Created by Sandy House on 2020-09-23.
+//  Copyright © 2020 sandzapps. All rights reserved.
+//
+
+import Foundation
+
+public class Test
+{
+    public let constant = "constant"
+}
